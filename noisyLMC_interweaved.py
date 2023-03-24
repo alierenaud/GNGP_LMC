84f11418b4563032eb12f684cb21b8f218cb131c
Renaud Alie <30876340+alierenaud@users.noreply.github.com>
2023-03-24 15:24:55 -0400

    Update noisyLMC_interweaved.py

--- noisyLMC_interweaved.py
+++ noisyLMC_interweaved.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
 
 
-random.seed(10)
+# random.seed(0)
 
 def vec(A):
     
@@ -122,8 +122,9 @@ def A_move_white(A_invV_current,Dm1_current,Dm1Y_current,sigma_A,mu_A):
 
 
 ### global parameters
-n = 500
-p = 2
+n = 1000
+# p = 2
+p = 3
 
 
 ### generate random example
@@ -131,10 +132,17 @@ p = 2
 locs = np.linspace(0, 1, n)
 
 
-A = np.array([[-1.,1.],
-              [1.,1.]])
-phis = np.array([5.,20.])
-taus_sqrt_inv = np.array([1.,1.]) * 0.5
+# A = np.array([[-1.,1.],
+#               [1.,1.]])
+# phis = np.array([5.,20.])
+# taus_sqrt_inv = np.array([1.,1.]) * 0.5
+
+
+A = np.array([[-1.,0.,1.],
+              [1.,0.,1.],
+              [0.,2.,0.]])
+phis = np.array([5.,10.,20.])
+taus_sqrt_inv = np.array([1.,1.,1.]) * 0.1
 
 
 # Y, V_true = rNLMC(A,phis,taus_sqrt_inv,locs, retV=True)
@@ -146,13 +154,18 @@ plt.plot(locs,V_true[0])
 plt.plot(locs,Y[0], '.', c="tab:blue", alpha=0.5)
 plt.plot(locs,V_true[1])
 plt.plot(locs,Y[1], '.', c="tab:orange", alpha=0.5)
+plt.plot(locs,V_true[2])
+plt.plot(locs,Y[2], '.', c="tab:green", alpha=0.5)
 
 plt.show()
 
 ### priors
 sigma_A = 1.
-mu_A = np.array([[sigma_A,sigma_A],
-                 [0.,0.]])
+# mu_A = np.array([[sigma_A,sigma_A],
+#                  [0.,0.]])
+mu_A = np.array([[sigma_A,sigma_A,sigma_A],
+                 [0.,0.,0.],
+                 [0.,0.,0.]])
 
 min_phi = 3.
 max_phi = 30.
@@ -190,7 +203,8 @@ b = 0.1
 Dists = distance_matrix(np.transpose(np.array([locs])),np.transpose(np.array([locs])))
 
 ### init and current state
-phis_current = np.array([5.,20.])
+# phis_current = np.array([5.,20.])
+phis_current = np.array([5.,10.,20.])
 Rs_current = np.array([ np.exp(-Dists*phis_current[j]) for j in range(p) ])
 Rs_inv_current = np.array([ np.linalg.inv(Rs_current[j]) for j in range(p) ])
 
@@ -200,14 +214,18 @@ V_current = random.normal(size=(p,n))*1
 VmY_current = V_current - Y
 VmY_inner_rows_current = np.array([ np.inner(VmY_current[j], VmY_current[j]) for j in range(p) ])
 
-A_current = np.array([[1.,1.],
-              [0.,1.]])
+# A_current = np.array([[1.,1.],
+#               [0.,1.]])
+A_current = np.array([[1.,1.,1.],
+                      [0.,1.,1.],
+                      [0.,0.,1.]])
 # A_current = np.identity(p)
 A_inv_current = np.linalg.inv(A_current)
 
 A_invV_current = A_inv_current @ V_current
 
-taus_current = 1/(np.array([1.,1.]) * 0.5)**2
+# taus_current = 1/(np.array([1.,1.]) * 0.5)**2
+taus_current = 1/(np.array([1.,1.,1.]) * 0.1)**2
 Dm1_current = np.diag(taus_current)
 Dm1Y_current = Dm1_current @ Y
 
@@ -222,7 +240,7 @@ sigma_slice = 1
 
 
 ### samples
-N = 10000
+N = 1000
 
 ### global run containers
 phis_run = np.zeros((N,p))
@@ -278,28 +296,33 @@ print('Execution time:', (et-st)/60, 'minutes')
 
 print("Prior Means for Ranges", alphas / (alphas + betas) * range_phi + min_phi)
 
-tail = 2000
+tail = 400
 
 print('accept phi_1:',np.mean(acc_phis[0,tail:]))
 print('accept phi_2:',np.mean(acc_phis[1,tail:]))
-# print('accept phi_3:',np.mean(acc_phis[2,tail:]))
+print('accept phi_3:',np.mean(acc_phis[2,tail:]))
 
 plt.plot(phis_run[:,0])
 plt.plot(phis_run[:,1])
-# plt.plot(phis_run[:,2])
+plt.plot(phis_run[:,2])
 plt.show()
 
 print('mean phi_1:',np.mean(phis_run[tail:,0]))
 print('mean phi_2:',np.mean(phis_run[tail:,1]))
-# print('mean phi_3:',np.mean(phis_run[tail:,2]))
+print('mean phi_3:',np.mean(phis_run[tail:,2]))
 
 print('accept A:',np.mean(acc_A[tail:]))
 print('mean A:',np.mean(A_run[tail:],axis=0))
 
 plt.plot(A_run[:,0,0])
 plt.plot(A_run[:,0,1])
+plt.plot(A_run[:,0,2])
 plt.plot(A_run[:,1,0])
 plt.plot(A_run[:,1,1])
+plt.plot(A_run[:,1,2])
+plt.plot(A_run[:,2,0])
+plt.plot(A_run[:,2,1])
+plt.plot(A_run[:,2,2])
 plt.show()
 
 
@@ -307,31 +330,37 @@ plt.show()
 
 print('mean tau_1:',np.mean(taus_run[tail:,0]))
 print('mean tau_2:',np.mean(taus_run[tail:,1]))
+print('mean tau_2:',np.mean(taus_run[tail:,2]))
 
 print('real taus:',taus_sqrt_inv ** (-2))
 
 print('mean inv sqrt tau_1:',np.mean(1/np.sqrt(taus_run[tail:,0])))
 print('mean inv sqrt tau_2:',np.mean(1/np.sqrt(taus_run[tail:,1])))
+print('mean inv sqrt tau_2:',np.mean(1/np.sqrt(taus_run[tail:,2])))
 
 print('real sqrt inv taus:',taus_sqrt_inv)
 
 plt.plot(taus_run[:,0])
 plt.plot(taus_run[:,1])
+plt.plot(taus_run[:,2])
 # plt.plot(1/np.sqrt(taus_run[:,2]))
 plt.show()
 
 
 plt.plot(1/np.sqrt(taus_run[:,0]))
 plt.plot(1/np.sqrt(taus_run[:,1]))
+plt.plot(1/np.sqrt(taus_run[:,2]))
 # plt.plot(1/np.sqrt(taus_run[:,2]))
 plt.show()
 
 for i in range(N):
-    if i % 1000 == 0:
+    if i % 100 == 0:
         plt.plot(locs,V_run[i,0])
         plt.plot(locs,Y[0], '.', c="tab:blue", alpha=0.5)
         plt.plot(locs,V_run[i,1])
         plt.plot(locs,Y[1], '.', c="tab:orange", alpha=0.5)
+        plt.plot(locs,V_run[i,2])
+        plt.plot(locs,Y[2], '.', c="tab:green", alpha=0.5)
 
         plt.show()
 
@@ -339,17 +368,27 @@ plt.plot(A_run[tail:,0,0])
 plt.show()
 plt.plot(A_run[tail:,0,1])
 plt.show()
+plt.plot(A_run[tail:,0,2])
+plt.show()
 plt.plot(A_run[tail:,1,0])
 plt.show()
 plt.plot(A_run[tail:,1,1])
 plt.show()
+plt.plot(A_run[tail:,1,2])
+plt.show()
+plt.plot(A_run[tail:,2,0])
+plt.show()
+plt.plot(A_run[tail:,2,1])
+plt.show()
+plt.plot(A_run[tail:,2,2])
+plt.show()
 
 
-arr = np.zeros((N-tail,p**2))
+# arr = np.zeros((N-tail,p**2))
 
-arr[:,0] = A_run[tail:,0,0]
-arr[:,1] = A_run[tail:,0,1]
-arr[:,2] = A_run[tail:,1,0]
-arr[:,3] = A_run[tail:,1,1]
+# arr[:,0] = A_run[tail:,0,0]
+# arr[:,1] = A_run[tail:,0,1]
+# arr[:,2] = A_run[tail:,1,0]
+# arr[:,3] = A_run[tail:,1,1]
 
-np.savetxt('output.csv', arr, delimiter=',')
+# np.savetxt('output.csv', arr, delimiter=',')
