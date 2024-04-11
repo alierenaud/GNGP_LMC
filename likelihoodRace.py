c64611f6e787224e3f48d552ac0fe5da0881f8e2
Renaud Alie <30876340+alierenaud@users.noreply.github.com>
2024-04-11 13:46:28 -0400

    Update likelihoodRace.py

--- likelihoodRace.py
+++ likelihoodRace.py
@@ -81,19 +81,19 @@ likes_n = np.zeros(N)
 bs = np.zeros(n+1,dtype=object)
 rs = np.zeros(n+1)
 
-
+phi = (10+random.normal(size=N))*1
 
 st = time.time()
 
 for j in range(N):
 
-    phi = (10+random.normal())*1
+    
     
     for i in range(n+1):
     
-        Cnei_inv = np.linalg.inv(matern_kernel(Dists[Nei[i]][:,Nei[i]],phi))
+        Cnei_inv = np.linalg.inv(matern_kernel(Dists[Nei[i]][:,Nei[i]],phi[j]))
         
-        Cnei_i = matern_kernel(Dists[Nei[i],i],phi)
+        Cnei_i = matern_kernel(Dists[Nei[i],i],phi[j])
         
         b = Cnei_inv @ Cnei_i
         
@@ -133,13 +133,13 @@ st = time.time()
 
 for j in range(N):
 
-    phi = (10+random.normal())*0.01
+    
     
     for i in range(m+1):
     
-        Cnei_inv = np.linalg.inv(matern_kernel(Dists[Nei[i]][:,Nei[i]],phi))
+        Cnei_inv = np.linalg.inv(matern_kernel(Dists[Nei[i]][:,Nei[i]],phi[j]))
         
-        Cnei_i = matern_kernel(Dists[Nei[i],i],phi)
+        Cnei_i = matern_kernel(Dists[Nei[i],i],phi[j])
         
         b = Cnei_inv @ Cnei_i
         
@@ -162,6 +162,120 @@ print("Total Time:", (et-st)/60, "minutes")
 
 
 
-#### WITH GRID LOCATIONS???
+#### WITH GRID LOCATIONS
 
 
+n_obs = 1000
+loc_obs = random.uniform(size=n_obs)
+
+### compute grid neighbors
+
+gNei = np.zeros((n_obs,m),dtype=int)
+
+for i in range(n_obs):
+    
+    left_nei = int(np.floor(loc_obs[i]*n))
+    
+    left_lim = left_nei-m/2+1
+    right_lim = left_nei+m/2+1
+    
+    if left_lim < 0:
+        gNei[i] = np.arange(0,m)
+    elif right_lim > n+1:
+        gNei[i] = np.arange(n+1-m,n+1)
+    else:
+        gNei[i] = np.arange(left_lim,right_lim)
+        
+        
+    
+# for i in range(n_obs):
+    
+#     fig, ax = plt.subplots()
+
+#     ax.set_aspect(1)
+#     plt.scatter(locs[:],np.ones(n+1)*0.5, c="black")
+#     plt.scatter(loc_obs[i],0.5, c="tab:orange")
+#     plt.scatter(locs[gNei[i]],np.ones(m)*0.5, c="tab:green")
+#     plt.show()    
+    
+
+
+
+Distm = distance_matrix(locst[0:m,:], locst[0:m,:])
+
+gDists = np.zeros((n_obs,m))
+
+for i in range(n_obs):
+    gDists[i] = distance_matrix([[loc_obs[i]]],locst[gNei[i]])[0]
+
+glikes_n = np.zeros(N)
+
+gbs = np.zeros((n_obs,m),dtype=object)
+grs = np.zeros(n_obs)
+
+w_obs = np.ones(n_obs)    
+
+
+st = time.time()
+
+for j in range(N):
+    
+    
+
+    for i in range(n_obs):
+
+        Cnei_inv = np.linalg.inv(matern_kernel(Distm,phi[j]))
+        
+        Cnei_i = matern_kernel(gDists[i],phi[j])
+        
+        b = Cnei_inv @ Cnei_i
+        
+        gbs[i] = b
+        
+        grs[i] = 1 - np.transpose(Cnei_i)@b
+
+
+
+    glikes_n[j] = - 1/2* np.sum([ (w_obs[ii]  - np.inner(gbs[ii],w[gNei[ii]]))**2/grs[ii] + np.log(grs[ii])  for ii in range(n_obs)]) 
+
+et = time.time()
+
+print("Total Time:", (et-st)/60, "minutes")
+
+
+### smart implementation
+
+glikes_s = np.zeros(N)
+
+gbs = np.zeros((n_obs,m),dtype=object)
+grs = np.zeros(n_obs)
+
+
+
+st = time.time()
+
+for j in range(N):
+    
+    
+    
+    Cnei_inv = np.linalg.inv(matern_kernel(Distm,phi[j]))
+
+    for i in range(n_obs):
+
+        
+        
+        Cnei_i = matern_kernel(gDists[i],phi[j])
+        
+        b = Cnei_inv @ Cnei_i
+        
+        gbs[i] = b
+        
+        grs[i] = 1 - np.transpose(Cnei_i)@b
+
+
+
+    glikes_s[j] = - 1/2* np.sum([ (w_obs[ii]  - np.inner(gbs[ii],w[gNei[ii]]))**2/grs[ii] + np.log(grs[ii])  for ii in range(n_obs)]) 
+
+et = time.time()
+
+print("Total Time:", (et-st)/60, "minutes")
