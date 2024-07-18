82fbb0fcda112f0d5c5c4167a942056c79a71812
Renaud Alie <30876340+alierenaud@users.noreply.github.com>
2024-07-17 20:15:13 -0400

    tree plots

--- allTreePlots.py
+++ allTreePlots.py
@@ -9,6 +9,7 @@ import numpy as np
 from numpy import random
 import matplotlib.pyplot as plt
 
+random.seed(0)
 
 tab_cols = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
 
@@ -25,11 +26,11 @@ n_whiteoak = whiteoak.shape[0]
 n_redoak = redoak.shape[0]
 n_blackoak = blackoak.shape[0]
 
-X_obs = np.concatenate((maple,hickory,whiteoak,redoak,blackoak))
-Y_obs = np.concatenate((np.ones(n_maple,dtype=int)*1,np.ones(n_hickory,dtype=int)*2,np.ones(n_whiteoak,dtype=int)*3,np.ones(n_redoak,dtype=int)*4,np.ones(n_blackoak,dtype=int)*5))
+# X_obs = np.concatenate((maple,hickory,whiteoak,redoak,blackoak))
+# Y_obs = np.concatenate((np.ones(n_maple,dtype=int)*1,np.ones(n_hickory,dtype=int)*2,np.ones(n_whiteoak,dtype=int)*3,np.ones(n_redoak,dtype=int)*4,np.ones(n_blackoak,dtype=int)*5))
 
-# X_obs = np.concatenate((maple,hickory))
-# Y_obs = np.concatenate((np.ones(n_maple,dtype=int)*1,np.ones(n_hickory,dtype=int)*2))
+X_obs = np.concatenate((maple,hickory))
+Y_obs = np.concatenate((np.ones(n_maple,dtype=int)*1,np.ones(n_hickory,dtype=int)*2))
 
 
 fig, ax = plt.subplots()
@@ -39,10 +40,20 @@ ax.set_box_aspect(1)
 
 s = 20
 
+ax.scatter(maple[:,0],maple[:,1],s=s,c=tab_cols[0])
+ax.scatter(hickory[:,0],hickory[:,1],s=s,c=tab_cols[1])
+# ax.scatter(whiteoak[:,0],whiteoak[:,1],s=s,c=tab_cols[2])
+# ax.scatter(redoak[:,0],redoak[:,1],s=s,c=tab_cols[3])
+# ax.scatter(blackoak[:,0],blackoak[:,1],s=s,c=tab_cols[4])
+
 for i in random.permutation(X_obs.shape[0]):
 
     ax.scatter(X_obs[i,0],X_obs[i,1],s=s,c=tab_cols[Y_obs[i]-1])
 
+
+plt.legend(["maple", "hickory"], bbox_to_anchor=(1,0.8)) 
+# plt.legend(["maple", "hickory","whitoak","redoak","blackoak"], bbox_to_anchor=(1,0.8)) 
+# plt.savefig("Tree2.pdf", format="pdf", bbox_inches="tight")
 plt.show()
     
 # ax.scatter(maple[:,0],maple[:,1],s=s,c=tab_cols[0])
